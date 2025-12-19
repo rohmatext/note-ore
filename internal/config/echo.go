1552f0834e0d2220e63343d105f71494b7d373bb
Muhammad Rohmat Abdullah <rohmatext@gmail.com>
2025-12-19 08:00:15 +0700

    update CORS configuration

--- internal/config/echo.go
+++ internal/config/echo.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"strings"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
@@ -11,8 +13,11 @@ func NewEcho(config *viper.Viper) *echo.Echo {
 
 	app.Use(middleware.Logger())
 	app.Use(middleware.Recover())
+
+	allowOrigins := strings.Split(config.GetString("FRONTEND_URLS"), ",")
 	app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
+		AllowOrigins:     allowOrigins,
+		AllowCredentials: true,
 	}))
 
 	return app
